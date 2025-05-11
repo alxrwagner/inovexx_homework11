49b5bd2c99066d1985fb52cdff6e8ae76fb5f41b
Alexandr <alxr.wagner@gmail.com>
2025-05-11 19:22:39 +0300

    Homework11: started

---
+++ src/main/java/org/example/inovexx_spring/InovexxSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.inovexx_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InovexxSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InovexxSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/inovexx_spring/InovexxSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.inovexx_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InovexxSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
